ce9934e95fb2c541e7c63fe136f322f2d965a389
Wen Da Lu <wendalu@gmail.com>
2024-09-06 16:22:24 -0400

    Fixed bug computing the alpha calib with NODDI

--- tardiflab/scripts/01_processing/MVFcalc_scripts/get_alphaNODDI.m
+++ tardiflab/scripts/01_processing/MVFcalc_scripts/get_alphaNODDI.m
@@ -7,24 +7,31 @@ function alpha = get_alpha(tmp,list)
     subject = table2cell(subject);
 
     mtsat = [];
-    fraction = [];
+    IC_fraction = [];
+    FW_fraction = [];
 
     for a = 1:height(subject)
         sub = char(subject(a,1));
         FA = niftiread(strcat(tmp,'/MVFNODDI_calc/',sub,'_space-dwi_FA_MNI152_1mm_splenium.nii.gz'));
         IC = niftiread(strcat(tmp,'/MVFNODDI_calc/',sub,'_space-dwi_ICVF_MNI152_1mm_splenium.nii.gz'));
+        FW = niftiread(strcat(tmp,'/MVFNODDI_calc/',sub,'_space-dwi_FWF_MNI152_1mm_splenium.nii.gz'));
         MT = niftiread(strcat(tmp,'/MVFNODDI_calc/',sub,'_space-dwi_MTsat_MNI152_1mm_splenium.nii.gz'));
 
         FA = nonzeros(FA);
         IC = nonzeros(IC);
         MT = nonzeros(MT);
+        FW = nonzeros(FW);
 
         mtsat = [mtsat transpose(MT(FA>0.8))];
-        fraction = [fraction transpose(IC(FA>0.8))];
+        IC_fraction = [IC_fraction transpose(IC(FA>0.8))];
+        FW_fraction = [FW_fraction transpose(FW(FA>0.8))]
+
     end 
 
     g = 0.7;
-    MVF = (-g^2.*fraction+fraction)./(g^2-g^2.*fraction+fraction);
+
+
+    MVF = (-g^2.*IC_fraction.*(1-FW_fraction)+IC_fraction.*(1-FW_fraction))./(g^2-g^2.*IC_fraction.*(1-FW_fraction)+IC_fraction.*(1-FW_fraction));
     alpha = MVF./mtsat;
      
     alpha = double(mean(alpha));
