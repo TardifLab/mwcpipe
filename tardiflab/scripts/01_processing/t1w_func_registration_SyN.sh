93178b871c2e7ff9c4b193e6bb5b286d8d12cf42
mnelson <mark.nelson3@mail.mcgill.ca>
2025-02-04 16:49:54 -0500

    MOD: metric MI --> CC; reduce warp SMOOTH & SHRINK

--- tardiflab/scripts/01_processing/t1w_func_registration_SyN.sh
+++ tardiflab/scripts/01_processing/t1w_func_registration_SyN.sh
@@ -26,40 +26,43 @@
 
   translation="[$fixed1,$moving,0]" 												# 0=geometric center; 1=center of mass; 2=origin
   w8_fixed1="1.0" 														# weights for cost function
-  sample_fixed1="0.25"														# Proportion of points to sample
+  sample_fixed1="0.25" 														# Proportion of points to sample
 
-  RIGIDCONVERG="1000x500x250x100"
+  RIGIDCONVERG="1000x500x250x0"
+  RIGIDTOL="1e-6"
   RIGIDSHRINK="8x4x2x1"
-  RIGIDSMOOTH="3x2x1x0"
+  RIGIDSMOOTH="3x2x1x0vox"
 
-  AFFINECONVERG="1000x500x250x100"
-  AFFINESHRINK="8x4x2x1"
-  AFFINESMOOTH="3x2x1x0"
+  AFFINECONVERG="500x250x150x50"
+  AFFINETOL="1e-6"
+  AFFINESHRINK="4x2x1x1"
+  AFFINESMOOTH="2x1x0x0vox"
 
-  SYNCONVERG="100x100x100"
+  SYNCONVERG="100x70x50x20"
   SYNTOL="1e-6"
-  SYNSHRINK="3x2x1"
-  SYNSMOOTH="2x1x0"
+  SYNSHRINK="2x1x1x1"
+  SYNSMOOTH="1x0x0x0vox"
+  SYNREGULARIZE="0.1"
+
 
 # Rigid + Affine + SyN
   antsRegistration --dimensionality 3 \
     --float 0 \
     --output "$func_SyN_str" \
     --interpolation BSpline[3] \
-    --use-histogram-matching 1 \
     --transform Rigid[0.1] \
-    --metric MI["$fixed1","$moving","$w8_fixed1",32,Regular,"$sample_fixed1"] \
-    --convergence ["$RIGIDCONVERG",1e-6,10] \
+    --metric CC["$fixed1","$moving","$w8_fixed1",4] \
+    --convergence ["$RIGIDCONVERG","$RIGIDTOL",10] \
     --shrink-factors "$RIGIDSHRINK" \
     --smoothing-sigmas "$RIGIDSMOOTH" \
     --transform Affine[0.1] \
-    --metric MI["$fixed1","$moving","$w8_fixed1",32,Regular,"$sample_fixed1"] \
-    --convergence ["$AFFINECONVERG",1e-6,10] \
+    --metric CC["$fixed1","$moving","$w8_fixed1",4] \
+    --convergence ["$AFFINECONVERG","$AFFINETOL",10] \
     --shrink-factors "$AFFINESHRINK" \
     --smoothing-sigmas "$AFFINESMOOTH" \
-    --transform SyN[0.1,3,0] \
-    --metric MI["$fixed1","$moving","$w8_fixed1",32,Regular,"$sample_fixed1"] \
-    --convergence ["$SYNCONVERG",1e-6,10] \
+    --transform SyN["$SYNREGULARIZE",3,0] \
+    --metric CC["$fixed1","$moving","$w8_fixed1",4] \
+    --convergence ["$SYNCONVERG","$SYNTOL",10] \
     --shrink-factors "$SYNSHRINK" \
     --smoothing-sigmas "$SYNSMOOTH" \
     --initial-moving-transform "$translation" \
